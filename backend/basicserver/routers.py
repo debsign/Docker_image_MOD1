29d9e307048ef74cecf4d448aa93b0aa0ea5c6b7
debsign <debora.rubio.soliva@gmail.com>
2023-07-08 14:04:39 +0200

    Add files via upload

---
+++ backend/basicserver/routers.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from ingredient.viewsets import IngredientViewSet
+
+router = DefaultRouter()
+router.register('ingredients', IngredientViewSet, basename='ingredient')
+
+urlpatterns = router.urls
+
+
